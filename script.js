81e7de5d7366602de59a43a1c57052a5ca6fa224
ME-YOU-SICK <136545118+ME-YOU-SICK@users.noreply.github.com>
2023-06-14 09:39:33 +0530

    script.js

--- script.js
+++ script.js
@@ -1,11 +1,6 @@
 // Get DOM elements
 const quoteElement = document.getElementById("quote");
 const generateButton = document.getElementById("generate-button");
-const likeButton = document.getElementById("like-button");
-const copyButton = document.getElementById("copy-button");
-
-// Array to store liked quotes
-let likedQuotes = [];
 
 // Array of sample quotes (replace with your own quotes)
 const quotes = [
@@ -23,31 +18,8 @@ function generateQuote() {
   quoteElement.textContent = randomQuote;
 }
 
-// Function to like a quote
-function likeQuote() {
-  const quote = quoteElement.textContent;
-  if (!likedQuotes.includes(quote)) {
-    likedQuotes.push(quote);
-    likeButton.textContent = "Liked!";
-    likeButton.disabled = true;
-  }
-}
-
-// Function to copy URL to the page
-function copyURL() {
-  const url = window.location.href;
-  navigator.clipboard.writeText(url).then(() => {
-    copyButton.textContent = "Copied!";
-    setTimeout(() => {
-      copyButton.textContent = "Copy URL";
-    }, 2000);
-  });
-}
-
-// Event listeners
+// Event listener for generate button
 generateButton.addEventListener("click", generateQuote);
-likeButton.addEventListener("click", likeQuote);
-copyButton.addEventListener("click", copyURL);
 
 // Initial quote generation
 generateQuote();
